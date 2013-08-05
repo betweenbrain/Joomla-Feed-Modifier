bf0634a1ef0fb709249918d9641db21379acceb8
Matt Thomas <matt@betweenbrain.com>
2013-08-05 12:18:40 -0400

    Initial working version

---
+++ feedmodifier.php
@@ -0,0 +1,41 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       feedmodifier.php
+ * Created    8/5/13 10:37 AM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+jimport('joomla.plugin.plugin');
+
+class plgSystemFeedmodifier extends JPlugin {
+
+	function plgSystemFeedmodifier(&$subject, $config) {
+		parent::__construct($subject, $config);
+
+		$this->app    = JFactory::getApplication();
+		$this->format = JRequest::getVar('format');
+	}
+
+	function onAfterRender() {
+
+		if ($this->app->isAdmin() || $this->format != "feed") {
+			return TRUE;
+		}
+
+		$author = htmlspecialchars($this->params->get('author'));
+		$buffer            = JResponse::getBody();
+
+		$pattern     = '/<author>[^>]*<\/author>/i';
+		$replacement = $author ? '<author>' . $author . '</author>' : '';
+		$buffer      = preg_replace($pattern, $replacement, $buffer);
+		preg_match($pattern, $buffer, $matches);
+
+		JResponse::setBody($buffer);
+
+		return TRUE;
+	}
+}
